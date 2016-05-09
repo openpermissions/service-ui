b372116cf6fdaad4b499563d9a3927ed30e48b65
Tessa Conway <tessa.conway@cde.catapult.org.uk>
2016-05-09 12:31:05 +0100

    Update auth endpoint in config

--- scripts/src/config.js
+++ scripts/src/config.js
@@ -2,6 +2,6 @@ module.exports = {
   api: {
     accounts: 'https://localhost:8006/v1/accounts',
     repository: 'https://localhost:8004/v1/repository',
-    authentication: 'https://localhost:8007/v1/authentication'
+    authentication: 'https://localhost:8007/v1/auth'
   }
 };
